b9e3db3a1146e3981ac5d2c2761304642e22ea4c
khloud <khlouda342@gmail.com>
2016-09-23 15:44:46 +0200

    movieapp

--- app/src/main/java/myapplication90/khloud/example/com/myapplication_demo2/MainActivity.java
+++ app/src/main/java/myapplication90/khloud/example/com/myapplication_demo2/MainActivity.java
@@ -6,23 +6,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity implements RAdapter.onClick{
-
+	  private boolean slide;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        slide = findViewById(R.id.movie_detail_fragment) != null;
     }
 
 
     @Override
     public void onclick(long id) {
-        Bundle bundle = new Bundle();
-        bundle.putLong("id", id);
+    	  Bundle bundle = new Bundle();
+          bundle.putLong("id", id);
 
+          if (slide) {
+              DetailFragmentActivity.Det detailActivityFragment = new DetailFragmentActivity.Det();
+              detailActivityFragment.setArguments(bundle);
 
-        Intent i = new Intent(this, DetailFragmentActivity.class);
-        i.putExtra("id", id);
-
-        startActivity(i);
-    }
+              getSupportFragmentManager().beginTransaction()
+                      .replace(R.id.movie_detail_fragment, detailActivityFragment)
+                      .commit();
+          } else {
+              Intent i = new Intent(this, DetailFragmentActivity.class);
+              i.putExtra("id", id);
+              startActivity(i);
+          }
+      }
 }
